bf7ffab19d111275f575f7765d591e60d1d63f97
Ypolites <manon.bonnafous@outlook.com>
2024-05-13 14:45:47 -0400

    création des routes

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://oumaimanaja:Lacapsule123@cluster-shared.881lpbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-shared/tickethack';
+const connectionString = 'mongodb+srv://manon:Lacapsule123@cluster-shared.881lpbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-shared/Tickethack';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
- .then(() => console.log('Database connected'))
-
-  .catch(error => console.error(error));
+    .then(() => console.log('Database connected'))
+    .catch(error => console.error(error));
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
+const Trip = require('../models/trips');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// PAGE HOME
+/* POST/GET - Récupérer les trajets dans la base de donnée "trips" en fonction de Arrival / Departure / Date */
+router.get('/trips', (req, res) => {
+  Trip.find().then(trips => {
+    console.log(trips)
+    //res.json({ trips: trips })
+  });
 });
 
 module.exports = router;
