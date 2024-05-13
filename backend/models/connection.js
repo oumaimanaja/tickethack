b6234eae5bab1808a4cdafa425e51fe850c9e265
oumaima <oumaima.naja@gmail.com>
2024-05-13 20:04:45 +0200

    1st commit

---
+++ backend/models/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://oumaimanaja:Lacapsule123@cluster-shared.881lpbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-shared/tickethack';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+ .then(() => console.log('Database connected'))
+
+  .catch(error => console.error(error));
