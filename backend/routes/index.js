85d56a84a49066e5f4f205f2acbfe2b105e7efe2
oumaima <oumaima.naja@gmail.com>
2024-05-15 19:12:29 +0200

    fonction GetTime

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,37 +11,51 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+/// Fonction Get Time from Date
+function GetTimeFromDate(date) {
+  date = moment(date).format();
+  date = String(date);
+  console.log(date);
+  date = new Date(date);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  /*console.log(
+    `${hours % 12}:${minutes < 10 ? "0" : ""}${minutes} ${
+      hours >= 12 ? "PM" : "AM"
+    }`
+  );*/
+  return `${hours % 12}:${minutes < 10 ? "0" : ""}${minutes} ${
+    hours >= 12 ? "PM" : "AM"
+  }`;
+}
+
+
 // PAGE HOME
 /* POST/GET - Récupérer les trajets dans la base de donnée "trips" en fonction de Arrival / Departure / Date */
 router.get("/trips/:departure/:arrival/:date", (req, res) => {
   const { departure, arrival, date } = req.params;
-  const startDay = moment(date).format();
-  const endDay = moment(startDay).add(1, "days").format();
+  const startDay = moment(new Date(date)).format();
+  const endDay = moment(new Date(startDay)).add(1, "days").format();
 
   Trip.find({
     departure: departure,
     arrival: arrival,
-    date: { $gte: startDay, $lt: endDay },
+    //date: { $gte: startDay, $lt: endDay },
   }).then((data) => {
     const result = data;
-    for (let i = 0; i < result.length; i++) {
-      let newDate = result[i].date.getHours();
 
-      console.log(`newDate = ${newDate}`);
-      result[i].date = moment(newDate).format("'HH:mm A'");
-      console.log(`result = ${result[i].date}`);
+    for (let i = 0; i < data.length; i++) {
+      result[i].dateFormated = GetTimeFromDate(data[i].date);
+      console.log(result[i].dateFormated);
     }
-    res.json({ tripsaa: result });
+    console.log(data)
+    res.json({data});
   });
-});
+;
+})
 
-function GetTimeFromDate(date) {
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  return `${hours % 12}:${minutes < 10 ? '0' : ''}${minutes} ${hours >= 12 ? 'PM' : 'AM'}`
-}
-console.log(GetTimeFromDate(moment("2024-05-13T18:10:55.985Z")))
+console.log(GetTimeFromDate(new Date("2024-05-13T18:10:55.985Z")));
 /*router.get('/trips', (req, res) => {
   Trip.find().then(data => {
     res.json({ trips: data })
