858a597412f90f6bd2313847024f04765962d685
Ypolites <manon.bonnafous@outlook.com>
2024-05-16 14:59:07 -0400

    ajout route toBook

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,7 +12,7 @@ router.get("/", function (req, res, next) {
 });
 
 // PAGE HOME
-/* POST/GET - Récupérer les trajets dans la base de donnée "trips" en fonction de Arrival / Departure / Date */
+/* GET - Récupérer les trajets dans la base de donnée "trips" en fonction de Arrival / Departure / Date */
 router.get("/trips/:departure/:arrival/:date", (req, res) => {
   const { departure, arrival, date } = req.params;
   const startDay = moment(new Date(date)).format();
@@ -24,10 +24,11 @@ router.get("/trips/:departure/:arrival/:date", (req, res) => {
       for (let i = 0; i < data.length; i++) {
         const departure = data[i].departure;
         const arrival = data[i].arrival;
+        const date = data[i].date;
         const hourMin = moment(data[i].date).format('LT');
         const price = data[i].price;
 
-        let newTrip = { departure: departure, arrival: arrival, date: hourMin, price: price };
+        let newTrip = { departure: departure, arrival: arrival, date: date, hour: hourMin, price: price };
         result.push(newTrip);
       }
       if (result.length!=0){
@@ -41,18 +42,21 @@ router.get("/trips/:departure/:arrival/:date", (req, res) => {
   });
 ;
 
-
-
 // PAGE HOME
-/* POST - Ajoute un trip
-router.post('/new', (req, res) => {
-  const newTrip = new Trip ({
-    departure: 'Toulouse',
-    arrival: 'Paris',
-    date: new Date(),
-    price: 125,
-   });
-   newTrip.save().then(data => res.json({trip: data}));
-})*/
+/* PUT - Mettre un ticket en toBook: true dans la BdD */
+router.put("/trips/:departure/:arrival/:date", (req, res) => {
+  const { departure, arrival, date } = req.params;
+  console.log(date);
+  
+  Trip.updateMany(
+    { departure: departure, arrival: arrival, date: date },
+    { toBook: false }
+  ).then(() => {
+      Trip.find({departure: departure, arrival: arrival, date: date}).then(data => {
+        console.log(data);
+        res.json({data})
+      });
+  });
+});
 
 module.exports = router;
